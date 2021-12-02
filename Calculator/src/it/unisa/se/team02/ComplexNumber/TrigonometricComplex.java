b022aaed36723e26ed287770ee181db21adaa117
Marco <Marco@DESKTOP-SCE5RIG.mshome.net>
2021-12-02 17:18:48 +0100

    First TrigonometricComplex class implementation

---
+++ Calculator/src/it/unisa/se/team02/ComplexNumber/TrigonometricComplex.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.se.team02.ComplexNumber;
+
+/**
+ *
+ * @author Team02
+ */
+public class TrigonometricComplex implements Complex{
+    
+    private double r;
+    private double i;
+    private double module;
+    private double fi;
+
+    public TrigonometricComplex(double r, double i) {
+        this.r = r;
+        this.i = i;
+        this.module = module();
+        this.fi = fi();
+    }
+
+    public double module() {
+        return Math.sqrt(Math.pow(this.r, 2) + Math.pow(this.i, 2));
+    }
+
+    public double fi() {
+        if (r == 0 && i > 0) {
+            return Math.PI / 2;
+        } else if (r == 0 && i < 0) {
+            return -Math.PI / 2;
+        } else if (r > 0) {
+            return Math.atan(i / r);
+        } else if (r < 0 && i >= 0) {
+            return Math.atan(i / r) + Math.PI;
+        } else if (r < 0 && i < 0) {
+            return Math.atan(i / r) - Math.PI;
+        }
+        return -1;
+    }
+
+    @Override
+    public double getReal() {
+        return r;
+    }
+
+    @Override
+    public double getImg() {
+        return i;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + String.format("%.2f", r) + "+" + String.format("%.2f", i) + "j)";
+    }
+}
