4b9056bac29ad80ea3ac46ecc8d5988f60ea01e9
Francesco <Francesco@LAPTOP-CMMJO8Q5.mshome.net>
2021-12-02 17:14:26 +0100

    Add interface Complex

---
+++ Calculator/src/it/unisa/se/team02/ComplexNumber/Complex.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package it.unisa.se.team02.complexNumber;
+
+/**
+ *
+ * @author team02
+ */
+public interface Complex {
+
+    public double getReal();
+
+    public double getImg();
+
+    public String toString();
+
+}
