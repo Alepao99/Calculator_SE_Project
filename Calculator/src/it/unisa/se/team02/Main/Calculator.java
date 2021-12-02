149853729278ed2f7fecf226fff10e667978b7ef
Alepao99 <alessandropaolillo67@gmail.com>
2021-12-02 16:39:49 +0100

    Modification main Creation file css creation img

--- Calculator/src/it/unisa/se/team02/Main/Calculator.java
+++ Calculator/src/it/unisa/se/team02/Main/Calculator.java
@@ -4,25 +4,30 @@ package it.unisa.se.team02.Main;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
  */
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author Team02
  */
 public class Calculator extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
+        stage.setResizable(true);
         stage.setTitle("Calculator Complex");
+        stage.getIcons().add(new Image(getClass().getResource("/it/unisa/se/team02/StyleCalculator/Calculator-icon.png").toExternalForm()));
+        scene.getStylesheets().add("/it/unisa/se/team02/StyleCalculator/button_style.css");
+        stage.initStyle(StageStyle.UTILITY);
         stage.setScene(scene);
         stage.show();
     }
@@ -33,5 +38,5 @@ public class Calculator extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
