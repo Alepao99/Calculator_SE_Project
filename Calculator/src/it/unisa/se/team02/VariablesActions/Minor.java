cb329f43cefcaf0bf59270c229231bfc31b4eb5b
Alepao99 <alessandropaolillo67@gmail.com>
2021-12-06 09:22:33 +0100

    update package and import

--- Calculator/src/it/unisa/se/team02/VariablesActions/Minor.java
+++ Calculator/src/it/unisa/se/team02/VariablesActions/Minor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package it.unisa.se.team02.Main.VariablesActions;
+package it.unisa.se.team02.VariablesActions;
 
 import it.unisa.se.team02.ComplexNumber.CartesianComplex;
 import it.unisa.se.team02.ObservableStack.ObservableStack;
