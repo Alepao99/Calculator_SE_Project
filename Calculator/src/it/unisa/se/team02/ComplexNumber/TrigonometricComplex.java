cc005a705f0d57d48cfb1c9fc3b923513490690b
Alepao99 <alessandropaolillo67@gmail.com>
2021-12-11 12:11:01 +0100

    CReation Atan, ACos class and implement method

--- Calculator/src/it/unisa/se/team02/ComplexNumber/TrigonometricComplex.java
+++ Calculator/src/it/unisa/se/team02/ComplexNumber/TrigonometricComplex.java
@@ -56,7 +56,7 @@ public class TrigonometricComplex implements Complex {
         }
         return list;
     }
-         
+
     /**
      * This method allows to obatin the log of a complex number
      *
@@ -66,7 +66,8 @@ public class TrigonometricComplex implements Complex {
     public CartesianComplex log() {
         return new CartesianComplex(Math.log(module), fi);
     }
-     /**
+
+    /**
      * This method allows to obatin the pow of a complex number
      *
      * @return a complex number that contains the results of the pow
@@ -94,18 +95,18 @@ public class TrigonometricComplex implements Complex {
         CartesianComplex num = new CartesianComplex(1, 0);
         //creo 1-z^2
         CartesianComplex nSub = num.subtract(nPow);
-        TrigonometricComplex x = new TrigonometricComplex(nSub.getReal(),nSub.getImg());
+        TrigonometricComplex x = new TrigonometricComplex(nSub.getReal(), nSub.getImg());
         //creo sqrt(1-z^2)
         List<CartesianComplex> nSqrt = x.sqrt();
         CartesianComplex nSqrt2 = nSqrt.get(0);
         //creo un numero complesso 0+1i
         CartesianComplex nNum = new CartesianComplex(0, 1);
         // creo il prodotto i*z
-        CartesianComplex nProd = nNum.multiply(new CartesianComplex(this.r,this.i));
+        CartesianComplex nProd = nNum.multiply(new CartesianComplex(this.r, this.i));
         // somma all'interno del log
         CartesianComplex nSum = nProd.add(nSqrt2);
         // log
-        TrigonometricComplex x2 = new TrigonometricComplex(nSum.getReal(),nSum.getImg());
+        TrigonometricComplex x2 = new TrigonometricComplex(nSum.getReal(), nSum.getImg());
         CartesianComplex nLog = x2.log();
         //creo un numero complesso 0-1i
         CartesianComplex nNum2 = new CartesianComplex(0, -1);
@@ -113,9 +114,73 @@ public class TrigonometricComplex implements Complex {
         CartesianComplex nFinal = nNum2.multiply(nLog);
         return nFinal;
     }
-    
-    
-    
+
+    /**
+     * This method allows to obatin the acos of a complex number
+     *
+     * @return a complex number that contains the results of the acos
+     *
+     */
+    public CartesianComplex acos() {
+        //acos(z) = -i (log(z + i (sqrt(1 - z2))))
+        // creo z
+        TrigonometricComplex numberT = new TrigonometricComplex(this.r, this.i);
+        // creo z^2
+        CartesianComplex nPow = numberT.pow(2);
+        // creo 1
+        CartesianComplex num = new CartesianComplex(1, 0);
+        //creo 1-z^2
+        CartesianComplex nSub = num.subtract(nPow);
+        TrigonometricComplex x = new TrigonometricComplex(nSub.getReal(), nSub.getImg());
+        //creo sqrt(1-z^2)
+        List<CartesianComplex> nSqrt = x.sqrt();
+        CartesianComplex nSqrt2 = nSqrt.get(0);
+        //creo un numero complesso 0+1i
+        CartesianComplex nNum = new CartesianComplex(0, 1);
+        // creo il prodotto i*(sqrt(1 - z2))
+        CartesianComplex nProd = nNum.multiply(nSqrt2);
+        // sommo z al prodotto di prima
+        CartesianComplex nSum = nProd.add(new CartesianComplex(this.r, this.i));
+        // faccio il log
+        TrigonometricComplex x2 = new TrigonometricComplex(nSum.getReal(), nSum.getImg());
+        CartesianComplex nLog = x2.log();
+        //creo un numero complesso 0-1i
+        CartesianComplex nNum2 = new CartesianComplex(0, -1);
+        // prodotto tra -i e tutto il resto
+        CartesianComplex nFinal = nNum2.multiply(nLog);
+        return nFinal;
+    }
+
+    /**
+     * This method allows to obatin the atan of a complex number
+     *
+     * @return a complex number that contains the results of the atan
+     *
+     */
+    public CartesianComplex atan() {
+        //atan(z) = (i/2) log((i + z)/(i - z))
+        // creo z
+        TrigonometricComplex numberT = new TrigonometricComplex(this.r, this.i);
+        //creo un numero complesso 0+1i
+        CartesianComplex nNum = new CartesianComplex(0, 1);
+        //creo i-z
+        CartesianComplex nSub = nNum.subtract(new CartesianComplex(this.r, this.i));
+        //creo i+z
+        CartesianComplex nSum = nNum.add(new CartesianComplex(this.r, this.i));
+        //divido i 2 numeri
+        CartesianComplex nDiv = nSum.divide(nSub);
+        // faccio il log
+        TrigonometricComplex x2 = new TrigonometricComplex(nDiv.getReal(), nDiv.getImg());
+        CartesianComplex nLog = x2.log();
+        //creo 2
+        CartesianComplex num = new CartesianComplex(2, 0);
+        //faccio i/2
+        CartesianComplex nDiv2 = nNum.divide(num);
+        //prodotto finale
+        CartesianComplex nFinal = nDiv2.multiply(nLog);
+        return nFinal;
+    }
+
     @Override
     public double getReal() {
         return r;
